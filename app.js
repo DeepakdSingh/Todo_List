6dc847830c5de750f304e5c47d465b16da375223
Deepak Singh <84958955+DeepakdSingh@users.noreply.github.com>
2021-09-21 12:31:05 +0530

    Added new app.js

---
+++ app.js
@@ -0,0 +1,156 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import _ from "lodash";
+
+const app = express();
+
+mongoose.connect("mongodb+srv://Deepak:deepaks9867@cluster0.yzmvm.mongodb.net/Todo?retryWrites=true&w=majority");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+
+
+const taskSchema = {
+    name: {
+        type: String,
+        maxlength: 50,
+        required: [true,"pls check your data entry, no name is specified."]
+    }
+};
+
+const listSchema = {
+    name: {
+        type: String,
+        maxlength: 50,
+        required: [true,"pls check your data entry, no name is specified."]
+    },
+    listItem: [taskSchema]
+};
+
+const TaskMaker = mongoose.model("task",taskSchema);
+const ListMaker = mongoose.model("list",listSchema);
+const intro1 = new TaskMaker({
+    name: " <-- Check To delete , To add (+)"
+});
+const intro2 = new TaskMaker({
+    name: " Url+/listname to create new list"
+});
+
+
+
+
+const date = new Date();
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
+};
+
+// This method takes locale(region) & options(format) as an argument.
+const day = date.toLocaleDateString('en-US',options);
+
+
+
+
+app.get("/", (req, res)=>{
+    TaskMaker.find((err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(result.length === 0){
+                TaskMaker.insertMany([intro1,intro2]).then(()=>res.redirect("/"));   
+            }else{
+                res.render('index',{header: day,Items: result});
+            }
+        }
+    });
+});
+
+
+app.get("/:customList",(req ,res)=>{
+    const listName = _.capitalize(req.params.customList);
+    ListMaker.findOne({name: listName},(err,found)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(!found){
+                const list = new ListMaker({
+                    name: listName
+                });
+                list.save(()=>{
+                    res.redirect("/"+listName);
+                });
+            }else{
+                res.render("index",{header: found.name, Items: found.listItem});
+                }
+        } 
+    });
+});
+
+
+app.post("/add",(req, res)=>{
+    const listName = req.body.button;
+    const item = req.body.item;
+    const newTask = new TaskMaker({
+        name: item
+    });
+
+    if(listName===day){
+        newTask.save(()=>{
+            res.redirect("/");
+        });
+    }else{
+        ListMaker.findOne({name: listName},(err,found)=>{
+            if(err){
+                console.log(err);
+            }else{
+                found.listItem.push(newTask);
+                found.save(()=>{
+                    res.redirect("/"+found.name);
+                });  
+            }
+        });  
+    }
+});
+
+
+app.post("/delete",(req, res)=>{
+    const listName = req.body.header;
+    const itemId = req.body.checked;
+
+    if(listName===day){
+        TaskMaker.deleteOne({_id: itemId},(err)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect("/");
+            }
+        });
+    }else{
+        ListMaker.findOneAndUpdate({name: listName},{$pull: {listItem: {_id: itemId}}},(err, result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect("/"+listName);
+            }
+        });
+    }  
+});
+
+
+
+app.use((err, req, res, next)=>{
+    if(err){
+        res.redirect("/");
+    }
+});
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("server is running on port 3000");
+});
+
+
